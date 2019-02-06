a19759e9107fd18943da45ce0ae3fc9ec36f6405
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-02-06 15:48:53 +0700

    Spring Boot, CRUD

---
+++ src/main/java/com/okta/springbootvue/Todo.java
@@ -0,0 +1,45 @@
+package com.okta.springbootvue;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Todo {
+    @Id @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String title;
+
+    private Boolean completed = false;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}
